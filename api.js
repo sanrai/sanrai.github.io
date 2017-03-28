40b39976b7404d48526cf42dbbbd4a6db25f6b58
Sanjay Rai <sanrai@adobe.com>
2017-03-28 16:00:51 -0700

    test commit

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "subnav"
+    ],
+    "modules": [
+        "component"
+    ],
+    "allModules": [
+        {
+            "displayName": "component",
+            "name": "component",
+            "description": "Module that handles all helpx-specific overrides for the new Subnav.\n\nWiki here: https://wiki.corp.adobe.com/pages/viewpage.action?pageId=1214972615"
+        }
+    ],
+    "elements": []
+} };
+});
